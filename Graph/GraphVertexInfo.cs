c37dbf9b5281a224e23aa08878fe72090ea9e0bb
Eugene <zhekul.90@gmail.com>
2022-04-26 19:01:43 +0300

    add Dijkstra 's algorithm

---
+++ Graph/GraphVertexInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace BusRoute
+{
+    internal class GraphVertexInfo
+    {
+        public GraphVertex Vertex { get; set; }
+        public bool IsUnvisited { get; set; }
+        public int EdgesWeightSum { get; set; }
+        public GraphVertex? PreviousVertex { get; set; }
+
+        public GraphVertexInfo(GraphVertex vertex)
+        {
+            Vertex = vertex;
+            IsUnvisited = true;
+            EdgesWeightSum = int.MaxValue;
+            PreviousVertex = null;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace BusRoute
 {
@@ -12,6 +11,7 @@ namespace BusRoute
     /// </summary>
     public partial class MainWindow : Window
     {
+        Graph Graph = new Graph();
         public int StartPoint { get; set; }
         public int EndPoint { get; set; }
         public int StartTime { get; set; }
@@ -74,6 +74,8 @@ namespace BusRoute
                             Buses.ElementAt(i - Consts.MinRowInFile).Slings.Add(new Sprint(busStops[j], busStops[j + 1], travelTime[j]));
                         }
                         Buses.ElementAt(i - Consts.MinRowInFile).Slings.Add(new Sprint(busStops.Last(), busStops[0], travelTime.Last()));
+
+                        Buses.ElementAt(i - Consts.MinRowInFile).Stops = moves.Skip(1).Take(int.Parse(moves[0])).Select(s=> int.Parse(s)).ToArray();
                     }
                 }
             }
@@ -81,9 +83,121 @@ namespace BusRoute
 
         private async void CalculateButtonClick(object sender, RoutedEventArgs e)
         {
+            var cost = 0;
+            Graph = new();
             var startPoint = Convert.ToInt32(StartText.Text);
             var endPoint = Convert.ToInt32(EndText.Text);
             var startTime = Convert.ToInt32(Convert.ToDateTime(StartTimeText.Text).Hour * Consts.MinutesInHour + Convert.ToDateTime(StartTimeText.Text).Minute);
+
+
+            var buses = Buses.Where(b => b.CheckBusRoute(startPoint, endPoint) == true).ToList();
+
+            if (buses.Any())
+            {
+                cost = buses.Where(b => b.IsHasDirectRoute(startPoint, endPoint) == true).FirstOrDefault() is not null ?
+                    buses.Where(b => b.IsHasDirectRoute(startPoint, endPoint) == true).Min(b => b.Costs) : 0;
+
+                int StartStop = startPoint, FinalStop = endPoint;
+
+                for (int i = 1; i < 5; i++)
+                    Graph.AddVertex(i);
+                AddTimeEdge(startPoint, startTime);
+
+                var dijkstra = new Dijkstra(Graph).FindShortestPath(StartStop, FinalStop);
+                if (dijkstra == "")
+                {
+                    MessageBox.Show("Пути не существует");
+                }
+                else
+                {
+                    var s = dijkstra.Replace("=", "\nВремя в пути: ");
+                    MessageBox.Show(s);
+                }
+            }
+        }
+
+        void AddTimeEdge(int stop, int time)
+        {
+            var list = Buses.Where(x => x.Stops.Contains(stop)).ToList();
+            foreach (var x in list)
+            {
+                int nextstop = x.GetNextStop(stop);
+                var tmp = Graph.Vertices.FirstOrDefault(y => y.Name == stop);
+
+                int nexttime = x.GetTime(time, stop, nextstop);
+                if (nexttime == -1) continue;
+                if (tmp is not null
+                 && tmp.Edges.Any(y => (y.ConnectedVertex.Name == nextstop) && (y.EdgeWeight <= nexttime)))
+                    return;
+                Graph.AddEdge(stop, nextstop, nexttime);
+                AddTimeEdge(nextstop, time + nexttime);
+            }
+        }
+
+        void AddMoneyEdge(int stop, int time)
+        {
+            var list = Buses.Where(x => x.Stops.Contains(stop)).ToList();
+            foreach (var x in list)
+            {
+                var tmp = Graph.Vertices.FirstOrDefault(y => y.Name == stop);
+                List<int> temptime = new();
+                foreach (var s in x.Stops)
+                {
+                    int tm = x.GetTime(time, stop, s);
+                    if (tmp is not null && tmp.Edges.Any(y => y.ConnectedVertex.Name == s))
+                        tm = -1;
+                    temptime.Add(tm);
+                    if (s != stop && tm != -1)
+                        Graph.AddEdge(stop, s, x.Costs);
+                }
+                for (int i = 0; i < x.Stops.Length; i++)
+                {
+                    if (x.Stops[i] != stop && temptime[i] != -1)
+                        AddMoneyEdge(x.Stops[i], time + temptime[i]);
+                }
+            }
+        }
+
+        public class Chart
+        {
+            public List<Bus> Buses { get; set; }
+            public List<Sprint> Sprints { get; set; }
+
+            public Chart(List<Bus> buses)
+            {
+                Buses = buses;
+            }
+
+
+            public void CreateCostGraph(int start, int end)
+            {
+                foreach (var bus in Buses)
+                {
+
+
+                    //foreach (var s in Sprints)
+                    //{
+                    //    s.
+                    //}
+
+                    //if (bus.IsHasDirectRoute(start, end) == false)
+                    //{
+                    //    var temp = new List<Sprint>();
+                    //    foreach (var current in bus.Slings)
+                    //    {
+                    //        temp.Add(current);
+                    //    }
+                    //    //temp.Add(films);
+                    //    var nextBus = Buses.Except((IEnumerable<Bus>)bus).FirstOrDefault();
+                    //    // MovieTheater theater = new MovieTheater(TimeWork - films, temp);
+
+                    //    //buses.Any();
+                    //    var chart = new Chart(Buses);
+                    //    //Chart.Add(theater);
+                    //    chart.CreateCostGraph();
+                    //}
+                }
+            }
         }
 
         public class Bus
@@ -91,6 +205,8 @@ namespace BusRoute
             public int Id { get; set; }
             public int Costs { get; set; }
             public int StartTime { get; set; }
+            public int[] Times { get; set; }
+            public int[] Stops { get; set; }
             public List<Sprint> Slings { get; set; } = new List<Sprint>();
 
             public Bus(int id, int startTime, int costs)
@@ -99,40 +215,49 @@ namespace BusRoute
                 Costs = costs;
                 StartTime = startTime;
             }
+
+            public bool CheckBusRoute(int start, int end)
+            {
+                return Slings.Where(s => s.FromStation == start).Any() || Slings.Where(s => s.ToStation == end).Any();
+            }
+
+            public bool IsHasDirectRoute(int start, int end)
+            {
+                return Slings.Where(s => s.FromStation == start).Any() && Slings.Where(s => s.ToStation == end).Any();
+            }
+
+            public int GetTime(int currenttime, int start, int destination)
+            {
+                if (!Stops.Contains(destination) || !Stops.Contains(start)) return -1;
+                int time = StartTime;
+                int idx = Stops.ToList().IndexOf(destination);
+                int count = Stops.Length;
+                bool visited = false;
+                for (int i = 0; time < 1440 && (!visited || i % count != idx || currenttime > time); i++)
+                {
+                    if (Stops[i % count] == start && time >= currenttime)
+                        visited = true;
+                    time += Times[i % count];
+                }
+                if (time >= 1440) return -1;
+                return time - currenttime;
+            }
+
+            public int GetNextStop(int current) => Stops[(Stops.ToList().IndexOf(current) + 1) % Stops.Length];
         }
 
         public class Sprint
         {
-            public int StationFrom { get; set; }
-            public int StationTo { get; set; }
+            public int FromStation { get; set; }
+            public int ToStation { get; set; }
             public int TravelTime { get; set; }
 
             public Sprint(int stationFrom, int stationTo, int travelTime)
             {
-                StationFrom = stationFrom;
-                StationTo = stationTo;
+                FromStation = stationFrom;
+                ToStation = stationTo;
                 TravelTime = travelTime;
             }
         }
-
-        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        //{
-        //    TextBox textBox = (TextBox)sender;
-        //    StartPoint = Convert.ToInt32(textBox.Text);
-        //}
-
-        //private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        //{
-        //    TextBox textBox = (TextBox)sender;
-        //    EndPoint = Convert.ToInt32(textBox.Text);
-
-        //}
-
-        //private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
-        //{
-        //    TextBox textBox = (TextBox)sender;
-        //    //StartTime = Convert.ToInt32(textBox.Text);
-
-        //}
     }
 }
